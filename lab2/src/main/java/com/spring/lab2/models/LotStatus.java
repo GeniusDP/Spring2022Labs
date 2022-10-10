e8c64bceafef8634222e43511dd2ce31a3fd4bed
GeniusDP <zaranikbz@gmail.com>
2022-10-10 17:28:26 +0300

    Revert "Add a basic implementation of models, controllers, services, repositories"
    This reverts commit 292b5683d243ba7ee99c828114ca0956ea6e8874.

--- lab2/src/main/java/com/spring/lab2/models/LotStatus.java
+++
@@ -1,7 +0,0 @@
-package com.spring.lab2.models;
-
-public enum LotStatus {
-    OPEN,
-    CLOSED,
-    SOLD
-}
--- lab2/src/main/java/com/spring/lab2/repositories/BidRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.lab2.repositories;
-
-import com.spring.lab2.models.Bid;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BidRepository extends CrudRepository<Bid, String> {
-}
