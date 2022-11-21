e657d914ceb91bf3e4f95a882ef39d9a9da930b2
GeniusDP <zaranikbz@gmail.com>
2022-11-21 10:30:04 +0200

    added init bids

--- lab2/src/main/java/com/spring/lab2/Lab2Application.java
+++ lab2/src/main/java/com/spring/lab2/Lab2Application.java
@@ -1,5 +1,7 @@
 package com.spring.lab2;
 
+import com.spring.lab2.entities.Bid;
+import com.spring.lab2.entities.BidStatus;
 import com.spring.lab2.entities.Lot;
 import com.spring.lab2.entities.User;
 import com.spring.lab2.repositories.LotRepository;
@@ -25,18 +27,43 @@ public class Lab2Application implements CommandLineRunner {
     public void run(String... args) {
         User bogdan = new User("bogdan", "pass");
         Lot computer = Lot.builder().lotName("computer").owner(bogdan).startPrice(1300).build();
-        Lot mobilePhone = Lot.builder().lotName("mobile phone").owner(bogdan).startPrice(450).build();
+        Lot mobilePhone = Lot.builder()
+          .lotName("mobile phone")
+          .owner(bogdan)
+          .startPrice(450)
+          .build();
         lotRepository.save(computer);
         lotRepository.save(mobilePhone);
         bogdan.addLot(computer);
         bogdan.addLot(mobilePhone);
         userRepository.save(bogdan);
 
+
         User vlad = new User("vlad", "pass");
-        Lot springInActionBook = Lot.builder().lotName("Spring In Action 6-th Edition").owner(vlad).startPrice(70).build();
+        Lot springInActionBook = Lot.builder()
+          .lotName("Spring In Action 6-th Edition")
+          .owner(vlad)
+          .startPrice(70)
+          .build();
         vlad.addLot(springInActionBook);
         lotRepository.save(springInActionBook);
         userRepository.save(vlad);
+
+        Bid computerBid1 = Bid.builder()
+          .lot(computer)
+          .creator(vlad)
+          .bidStatus(BidStatus.NOT_PROCESSED)
+          .value(120)
+          .build();
+        Bid computerBid2 = Bid.builder()
+          .lot(computer)
+          .creator(vlad)
+          .bidStatus(BidStatus.ACCEPTED)
+          .value(140)
+          .build();
+        computer.addBid(computerBid1);
+        computer.addBid(computerBid2);
+
 
     }
 }
