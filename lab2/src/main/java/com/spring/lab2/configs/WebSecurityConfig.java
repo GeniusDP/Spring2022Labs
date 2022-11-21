e2be85e7916bc4f0d1d8547cc4870acec37da357
GeniusDP <zaranikbz@gmail.com>
2022-11-21 10:32:56 +0200

    allowed security /api endpoint

--- lab2/src/main/java/com/spring/lab2/configs/WebSecurityConfig.java
+++ lab2/src/main/java/com/spring/lab2/configs/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers("/api/*").permitAll()
                 .anyRequest().authenticated()
                 .and();
 
